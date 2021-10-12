4a57b4b6b0dab139aa3e200654ca13b8102b388d
Daniel Rau <drau88@cox.net>
2021-10-11 17:10:55 -0700

    Added functionality to Home link. Home link successfully returns the user to the main page.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 export default function Movie(props) {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
 
   let { id } = useParams();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
@@ -45,11 +45,11 @@ export default function Movie(props) {
         </div>
         <h3>Actors</h3>
 
-        {/* {stars.map(star => (
+        {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
-        ))} */}
+        ))}
       </div>
       <div className="save-button">Save</div>
     </div>
